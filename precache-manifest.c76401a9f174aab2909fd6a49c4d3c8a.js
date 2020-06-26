c5e69d0fcbdf95e9682f9bf42a7e22511f303644
sarab <sarabjeet.mannu.singh@gmail.com>
2020-06-26 02:42:00 -0300

    Updates

---
+++ precache-manifest.c76401a9f174aab2909fd6a49c4d3c8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6b33fb3c333ff146999376d4a8aff1",
+    "url": "/Live-Sortings/index.html"
+  },
+  {
+    "revision": "0f67d0707066d2f0dde6",
+    "url": "/Live-Sortings/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1f8a0f49d266e869c501",
+    "url": "/Live-Sortings/static/css/main.c4e466a5.chunk.css"
+  },
+  {
+    "revision": "0f67d0707066d2f0dde6",
+    "url": "/Live-Sortings/static/js/2.5a29eed8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Live-Sortings/static/js/2.5a29eed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8a0f49d266e869c501",
+    "url": "/Live-Sortings/static/js/main.b4714aac.chunk.js"
+  },
+  {
+    "revision": "af40d88b29842a0c2733",
+    "url": "/Live-Sortings/static/js/runtime-main.9367b772.js"
+  }
+]);
