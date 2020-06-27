f7752888ffe18396df31e8efef566af3cfafab27
sarab <sarabjeet.mannu.singh@gmail.com>
2020-06-26 21:41:36 -0300

    correcting footer link

--- src/Nav_Footer/Footer.jsx
+++ src/Nav_Footer/Footer.jsx
@@ -8,7 +8,7 @@ class Footer extends Component {
         <footer class="page-footer font-small unique-color-dark pt-4">
           <div class="footer-copyright text-center py-3">
             © 2020 Copyright:
-            <a href="/"> Live-Sortings</a>
+            <a href="/Live-Sortings"> Live-Sortings</a>
           </div>
         </footer>
       </>
